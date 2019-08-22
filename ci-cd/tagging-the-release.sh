838feea051ae4dcfaf270519f1b28f6057d70047
Avery Roswell (LCL) <avery.roswell@loblaw.ca>
2019-08-22 16:53:04 -0400

    Added logging for tagging script.

--- ci-cd/tagging-the-release.sh
+++ ci-cd/tagging-the-release.sh
@@ -5,6 +5,10 @@ set -e
 VERSION=$(< ./gitrise.sh grep -o "VERSION='[0-9]\{1,\}.[0-9]\{1,\}.[0-9]\{1,\}.*'" \
 | grep -o "[0-9]\{1,\}.[0-9]\{1,\}.[0-9]\{1,\}.*[^']")
 
+# logging
+git tag -l
+echo "Parsed version was $VERSION"
+
 # turning off exit on error
 set +e
 
